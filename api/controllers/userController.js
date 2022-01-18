b62ffdafb4b826aa3cd84cc290d0dd598f88fe6d
Surekha Shelake <shelakesurekha@gmail.com>
2022-01-18 09:43:36 +0530

    added middleware for headers check from FE

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,5 @@
 let db = require('../../db').localConnect();
-
+const jwt = require('jsonwebtoken');
 module.exports = {
 
     getUsers: () => {
@@ -21,19 +21,72 @@ module.exports = {
     regisitorNewUser: (req) => {
 
         return new Promise((resolve, reject) => {
-            let { username, email, password } = req; //object properrties destructuring syntax
+            //  let { username, email, password } = req; //object properrties destructuring syntax
 
-            let query = "INSERT INTO `users_register` (`id`, `username`, `email`, `password`) VALUES (NULL, '" + username + "','" + email + "', '" + password + "')";
 
-            db.query(query, (error, result, fields) => {
 
-                
-                if (error) {
-                    return reject(error);
-                } else {
-                    return resolve(result);
+            try {
+                // Get user input
+                const { username, email, password } = req;
+
+                // Validate user input
+                if (!(email && password && username)) {
+                    return reject("All input is required");
                 }
-            });
+
+
+                // // check if user already exist
+                // // Validate if user exist in our database
+                // const oldUser = await User.findOne({ email });
+
+                // if (oldUser) {
+                //     return res.status(409).send("User Already Exist. Please Login");
+                // }
+                //Encrypt user password
+                // encryptedPassword =  bcrypt.hash(password, 10);
+
+
+                let query = "INSERT INTO `users_register` (`id`, `username`, `email`, `password`) VALUES (NULL, '" + username + "','" + email + "', '" + password + "')";
+
+                db.query(query, (error, result, fields) => {
+
+
+                    if (error) {
+                        return reject(error);
+                    } else {
+
+                        // Create token
+                        const token = jwt.sign(
+                            { user_id: username },
+                            `${process.env.JWT_KEY}`,
+                            {
+                                expiresIn: "2h",
+                            }
+                        );
+                        // save user token
+                     
+
+                        let user = {
+                            token,
+                            username,
+                            email
+                        }
+                        
+                        return resolve(user);
+                    }
+                });
+
+
+
+            } catch (e) {
+                return reject(error);
+            }
+
+
+
+
+
+
 
         });
 
@@ -44,21 +97,35 @@ module.exports = {
 
         return new Promise((resolve, reject) => {
             let { username, password } = data; //object properrties destructuring syntax
+            // Validate user input
+            if (!(username && password)) {
+                return reject("All input is required");
+            }
 
             let query = "SELECT * FROM `users_register` WHERE username = '" + username + "' and password='" + password + "'";
 
             db.query(query, (error, result, fields) => {
                 if (result.length > 0) {
-                    return resolve(result);
+                    // Create token
+                    const token = jwt.sign(
+                        { user_id: username },
+                        `${process.env.TOKEN_KEY}`,
+                        {
+                            expiresIn: "2h",
+                        }
+                    );
+
+                    let user = {
+                        username,
+                        token
+                    }
+                    return resolve(user);
                 } else {
-                    return reject({
-                        "Error": "user not found.."
-                    });
+                    return reject("Invalid Credentials");
                 }
 
-            });
-
-        });
+            })
+        })
 
     }
 }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,19 +1,21 @@
 let db = require('../../db').localConnect();
 let express = require('express');
-
+const authMiddleware = require("../../middleware/auth");
 const routes = express.Router();
 
 let userController = require('../controllers/userController');
 
 //GET ->  api/user 
-routes.get("/", (request, response) => {
+routes.get("/", authMiddleware, (request, response) => {
+    // console.log(request.headers);
+
     userController.getUsers()
         .then(
             (success) => {
-                 console.log(success)
+                // console.log(success)
                 response.json(success)
 
-              //  response.status(401).json({ success: false, error: "Unathorized user" });
+                //    response.status(500).json({ success: false, error: "Unathorized user" });
             },
             (reject) => {
                 response.json(reject)
@@ -30,12 +32,13 @@ routes.post('/register', function (request, response) {
                 response.json(success)
             },
             (error) => {
-                response.json(error)
+                response.status(400).json(error)
             }
         )
 });
 
 //POST -> api/user/login
+//authMiddleware
 routes.post('/login', function (request, response) {
 
     userController.loginUser(request.body)
@@ -44,7 +47,7 @@ routes.post('/login', function (request, response) {
                 response.json(success)
             },
             (error) => {
-                response.json(error)
+                response.status(400).json(error)
             }
         )
 });
